950b4ad5f3d1e9eb72ee060c6e29245c151d7595
FZC <842693999@qq.com>
2020-12-24 16:34:22 +0800

    update evaluation

--- Eval.py
+++ Eval.py
@@ -8,7 +8,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(np.argmax(memory_gpu))
 os.system('rm tmp')
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, \
+    classification_report, confusion_matrix
 import argparse
 from Models import Resnet, VGG, GoogleNet
 from torchvision.datasets import ImageFolder
@@ -24,10 +25,9 @@ parser.add_argument('--model', '-m', type=str, default='res18', metavar='M',
                     help='type of model (res18, res34, vgg16 or googlenet)')
 parser.add_argument('--bsize', '-bs', type=int, default=8, metavar='S',
                     help='batch size of the train data (8, 16, 32, 64 or 128)')
-parser.add_argument('--pretrained', default='False', action='store_true',
-		                  help='eval finetuned pretrainded model')
+parser.add_argument('--pretrained', default=False, action='store_true',
+                    help='eval finetuned pretrainded model')
 args = parser.parse_args()
-print(args.model, args.bsize, args.pretrained)
 device = get_default_device()
 
 # set seed
@@ -49,9 +49,10 @@ elif args.model == 'googlenet':
     model = GoogleNet.GoogLeNet(num_classes=5, init_weights=True)
 else:
     exit(-1)
-# print(model)
+
 to_device(model, device)
-print('Using GPU:' + str(np.argmax(memory_gpu))) 
+print('Using GPU:' + str(np.argmax(memory_gpu)))
+
 # load data
 data_dir = './JZ20200509/'
 tfms = tt.Compose([
@@ -71,17 +72,18 @@ val_dl = DeviceDataLoader(valid_dl, device)
 
 print(args.pretrained)
 if args.pretrained is True:
-    model.load_state_dict(torch.load('./results/{}/Best-Model-bsize{}-pt.pth'.format(args.model, args.bsize))['state_dict'])
+    model.load_state_dict(
+        torch.load('./results/{}/Best-Model-bsize{}-pt.pth'.format(args.model, args.bsize))['state_dict'])
 else:
-    model.load_state_dict(torch.load('./results/{}/Best-Model-bsize{}.pth'.format(args.model, args.bsize))['state_dict'])
+    model.load_state_dict(
+        torch.load('./results/{}/Best-Model-bsize{}.pth'.format(args.model, args.bsize))['state_dict'])
 
 
 def step(model, batch):
     images, labels = batch
     out = model(images)  # Generate predictions
     preds = torch.max(out, dim=1)[1]
-    # print(torch.max(out, dim=1))
-    # acc = accuracy(out, labels)  # Calculate accuracy
+
     return {'preds': preds, 'labels': labels, 'out': out}
 
 
@@ -91,16 +93,17 @@ def evaluate(model, val_loader):
     outputs = [step(model, batch) for batch in val_loader]
     Preds = [x['preds'] for x in outputs]
     Labels = [x['labels'] for x in outputs]
-    Outs = [x['out'] for x in outputs]    
+    Outs = [x['out'] for x in outputs]
 
-    Preds = torch.cat(Preds,dim=0).cpu()
-    Labels = torch.cat(Labels,dim=0).cpu()
-    Outs = torch.cat(Outs,dim=0).cpu()
-    Scores = F.softmax(Outs,dim=1)    
+    Preds = torch.cat(Preds, dim=0).cpu()
+    Labels = torch.cat(Labels, dim=0).cpu()
+    Outs = torch.cat(Outs, dim=0).cpu()
+    Scores = F.softmax(Outs, dim=1)
 
     print(Preds, Labels, Scores)
     print(Preds.size(), len(Labels), Scores.size())
     print('General Evaluation')
+
     # Precision | Recall | F1 - score | AUC
     acc_all = accuracy_score(Labels, Preds)
     ap_all = precision_score(Labels, Preds, average='macro')
@@ -132,16 +135,17 @@ def evaluate(model, val_loader):
             y_true.append('PCV')
         elif Labels[i] == 4:
             y_true.append('PM')
-    t1 = classification_report(y_true, y_pred, # output_dict=True, 
-                              target_names=['AMD', 'DME', 'NM', 'PCV', 'PM'])
-    t2 = classification_report(y_true, y_pred, output_dict=True, target_names=['AMD', 'DME', 'NM', 'PCV', 'PM'])
+    t1 = classification_report(y_true, y_pred,
+                               target_names=['AMD', 'DME', 'NM', 'PCV', 'PM'])
+    t2 = classification_report(y_true, y_pred, output_dict=True,
+                               target_names=['AMD', 'DME', 'NM', 'PCV', 'PM'])
     print(t1)
     print(t2)
 
     # draw confuse matrix
     classes = ['AMD', 'DME', 'NM', 'PCV', 'PM']
     cm = confusion_matrix(y_true, y_pred)
-    fig,ax = plt.subplots()
+    fig, ax = plt.subplots()
     plt.imshow(cm, cmap=plt.cm.Greens)
     indices = range(len(cm))
     plt.xticks(indices, classes)
@@ -153,19 +157,16 @@ def evaluate(model, val_loader):
     for first_index in range(len(cm)):
         for second_index in range(len(cm[first_index])):
             plt.text(first_index, second_index, cm[first_index][second_index])
-    if args.pretrained:
-        fig.savefig("./img/{}/Best-cm-img{}-pt.png".format(args.model, args.bsize),
-                    dpi=320, format='png')
-    else:
-        fig.savefig("./img/{}/Best-cm-img{}.png".format(args.model, args.bsize),
+
+    fig.savefig("./img/{}/Best-cm-img{}.png".format(args.model, args.bsize),
                 dpi=320, format='png')
-    # print("accu: {:.4f}, ap: {:.4f}, ar: {:.4f}, f1_score: {:4f}".format(
-    #     acc_all, ap_all, ar_all, f1_all))
-    
+
     # cal auc
     roc_ovr = roc_auc_score(Labels, Scores, multi_class='ovr')
     print('--roc-ovr:', roc_ovr)
     roc_ovo = roc_auc_score(Labels, Scores, multi_class='ovo')
     print('--roc-ovo:', roc_ovo)
 
-evaluate(model, val_dl)
+
+if __name__ == "__main__":
+    evaluate(model, val_dl)
